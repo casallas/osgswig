1fd429a31aa3b31f3a004f7d0bca1e65ecd1fabd
gerwindehaan <none@none>
2008-07-11 15:45:20 +0000

    updated todaysimages sample with simplifying fixes
    --HG--
    extra : convert_revision : svn%3A9ea0355d-b33e-0410-9de5-817935624dc7/trunk%40141

--- examples/python/todaysimages.py
+++ examples/python/todaysimages.py
@@ -25,9 +25,10 @@ import osgDB
 import osgGA
 import osgViewer
 
-#Create a callback function for a transform node
 class RotateCB(osg.NodeCallback):
-    """Rotate UpdateCallback for Transform Nodes """
+    """
+    Rotate UpdateCallback for Transform Nodes 
+    """
     base_class = osg.NodeCallback
     def __init__(self, axis = osg.Vec3( 0.0, 0.0, 1.0 ), startangle = 0.0, speed=0.001):
         self._angle = startangle
@@ -37,36 +38,45 @@ class RotateCB(osg.NodeCallback):
         osg.NodeCallback.__init__(self)    
     def __call__(self, node, nv):
         """casts the transform node and rotate and increment angle"""
-        self._quat.makeRotate(self._angle, self._axis) 
-        #call the dynamic cast function that was added to the osg module
-        mt = osg.NodeToMatrixTransform(node)        
-        if mt:
-            mt.setMatrix(osg.Matrixd_rotate(self._quat))
-        else:            
-            pot = osg.NodeToPositionAttitudeTransform(node)
-            if pot:           
-                pot.setAttitude(self._quat)
-        
         # Increment the angle.        
         self._angle += self._speed;
-        # call traverse
+        # regenerate the quaternion
+        self._quat.makeRotate(self._angle, self._axis) 
+        #call the dynamic cast function that was added to the osg module
+        pot = osg.NodeToPositionAttitudeTransform(node)
+        if pot:           
+            pot.setAttitude(self._quat)
+        # call traverse for further osg processing
         self.traverse(node,nv)
 
-def getUrlFromRSS(pURL,pItem=0):
-    """ Get the BBC's Day in Images URL from the RSS 
+def getUrlsFromRSS(pURL,pItem=0):
+    """
+    Get the BBC's Day in Images URL from the RSS 
 
     pItem indicates which entry item one would like
     """
     try:
         url_info = urllib2.urlopen(pURL)
         xmldoc = minidom.parse(url_info)
-        return str(xmldoc.getElementsByTagName("item")[pItem].getElementsByTagName("link")[0].firstChild.wholeText)
+
+        #we're going to return a list of items
+        urllist = []
+
+        #build-up a list
+        for n in range(pItem):
+            #get the n-tn item tag, from that the text of the first link tag 
+            urllist+= [str(xmldoc.getElementsByTagName("item")[n].getElementsByTagName("link")[0].firstChild.wholeText)]
+
+        #return the list
+        return urllist
     except:
         print "Error getting the URL from the RSS"
         raise
 
 def getImageListFromPage(pUrl):
-    """ Get all files from <img> tags from the URL"""
+    """
+    Get all files from <img> tags from the URL
+    """
 
     #prepares for non-absolute img references
     lbaseurl = pUrl
@@ -78,7 +88,9 @@ def getImageListFromPage(pUrl):
     return imglistfiles
 
 def createObjectFromImageList(pNode, pImageList, pBaseUrl=""):
-    """uses the image list to create a set of objects"""
+    """
+    uses the image list to create a set of objects
+    """
 
     box_geometry = osg.Box()
     box_drawable = osg.ShapeDrawable(box_geometry)
@@ -106,45 +118,41 @@ def createObjectFromImageList(pNode, pImageList, pBaseUrl=""):
             q = osg.Quat()
             q.makeRotate(frandom(0,2*math.pi),0,0,1)
             lbox_node.setAttitude(q)   
-            lbox_node.setPosition  (osg.Vec3d(frandom(-50,50),
+            lbox_node.setPosition  (osg.Vec3d (frandom(-50,50),
                                                frandom(-50,50),
-                                               frandom(-100,100)
-                                               )   
-                                    )
-
+                                               frandom(-100,100)))
             pNode.addChild(lbox_node)
 
 #setup the scene with a rotating pillar
 root = osg.Group()
 pillar = osg.PositionAttitudeTransform()
-pillar.setUpdateCallback(RotateCB(speed=0.001).__disown__())
+animatecallback = RotateCB(speed=0.001)
+pillar.setUpdateCallback(animatecallback)
 root.addChild(pillar)
+
+imagelist=[]
 
 #get the BBC's "Today in images" page URL from today from the RSS
 rssaddress = 'http://newsrss.bbc.co.uk/rss/newsonline_world_edition/in_pictures/rss.xml'
 
-imagelist=[]
-#pick the last two days
-for i in range(2):
-    url = getUrlFromRSS(rssaddress,i)
-    #generate a list of images from the URL
-    imagelist += getImageListFromPage(url)
+#generate a list of URLs from the last couple of items
+urllist = getUrlsFromRSS(rssaddress,4)
+
+#generate a list of images from the list of URLs
+for urlitem in urllist:
+    imagelist += getImageListFromPage(urlitem)
 
 #create image objects from the list
 createObjectFromImageList(pillar,imagelist)
 
-# create a viewer
-viewer = osgViewer.Viewer()
-# configure
-viewer.setThreadingModel(osgViewer.Viewer.SingleThreaded)
 
-# convenience handlers
+
+# osgViewer section, see other viewer examples for these functions
+viewer = osgViewer.Viewer()
+viewer.setThreadingModel(osgViewer.Viewer.SingleThreaded)
 viewer.addEventHandler(osgViewer.StatsHandler())
 viewer.addEventHandler(osgViewer.WindowSizeHandler())
 viewer.addEventHandler(osgViewer.ThreadingHandler())
 viewer.addEventHandler(osgViewer.HelpHandler())
-
-# add to the scene
 viewer.setSceneData(root)
-# loop until done
 viewer.run()
